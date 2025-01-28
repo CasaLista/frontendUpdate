9b4ffebac0ba8a656084f3c9afb7ca00f0412ef4
lzugaz <93231358+lzugaz@users.noreply.github.com>
2025-01-28 15:47:58 -0500

    Listings removed from front end

--- frontend/src/components/Listings.vue
+++ frontend/src/components/Listings.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="listings-container">
-    
     <div class="map-container">
       <SiteHeader />
       <ListingFilters />
@@ -13,7 +12,6 @@
       <div ref="mapContainer" class="map"></div>
       <ListingFooter />
     </div>
-    <ListingPanel :listings="filteredListings" />
   </div>
 </template>
 
@@ -21,7 +19,6 @@
 import mapboxgl from 'mapbox-gl';
 import SiteHeader from "./SiteHeader.vue";
 import ListingFilters from './ListingFilters.vue';
-import ListingPanel from './ListingPanel.vue';
 import ListingFooter from './ListingFooter.vue';
 
 import { config } from '../../config';
@@ -30,19 +27,12 @@ export default {
   components: {
     SiteHeader,
     ListingFilters,
-    ListingPanel,
     ListingFooter,
   },
   data() {
     return {
       searchQuery: this.$route.query.search || '',
       map: null,
-      allListings: [
-        { id: 1, name: "Casa Bonita", price: "$200,000", coords: [-99.1332, 19.4326] },
-        { id: 2, name: "Villa Azul", price: "$350,000", coords: [-99.1400, 19.4400] },
-        { id: 3, name: "Penthouse MX", price: "$500,000", coords: [-99.1500, 19.4500] },
-      ],
-      filteredListings: [],
     };
   },
   watch: {
@@ -60,13 +50,9 @@ export default {
       zoom: 12,
     });
 
-    this.map.on('moveend', this.filterListingsByBounds);
-
     if (this.searchQuery) {
       this.searchLocation();
     }
-
-    this.filterListingsByBounds();  // Initial filtering
   },
   methods: {
     searchLocation() {
@@ -82,13 +68,6 @@ export default {
           .catch(error => console.error('Error fetching location:', error));
       }
     },
-    filterListingsByBounds() {
-      const bounds = this.map.getBounds(); // Get current map bounds
-      this.filteredListings = this.allListings.filter(listing => {
-        const [lng, lat] = listing.coords;
-        return bounds.contains([lng, lat]);
-      });
-    }
   },
 };
 </script>
@@ -99,7 +78,6 @@ export default {
   width: 100%;
   height: 100vh;
   font-family: 'CoFo Sans', sans-serif;
-
 }
 .map-container {
   display: flex;
@@ -118,11 +96,10 @@ input {
   z-index: 1;
   margin-top: 10px;
   font-family: 'CoFo Sans', sans-serif;
-
 }
+
 .map {
   width: 100%;
   height: calc(100% - 50px);
 }
-
 </style>
